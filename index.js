9b305163be8d05a535570d267e13495f0acfc365
tonovarela <tonovarela@live.com>
2023-07-12 17:27:57 -0600

    Se omite el uso de archivos

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import  {getFile, listarArchivosEnBucket, listarBuckets,subirArchivo} from "./helpers/helpers.js";  
+console.log("Antes");
+await listarArchivosEnBucket("litobucket");
+console.log("Despues ...");
+await getFile("archivo.pdf","litobucket")
 
 
-listarArchivosEnBucket("litobucket");
-///subirArchivo("litobucket","./archivos/archivo.pdf","archivo.pdf");
-getFile("archivo.pdf","litobucket")
 
